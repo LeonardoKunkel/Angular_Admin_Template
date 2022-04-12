050f028d439ad853735192f2620d2a1f69fa1bd8
Leonardo <mr_kunkel@hotmail.com>
2022-04-12 15:28:56 -0500

    Password validation

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,11 +11,13 @@ export class RegisterComponent {
   public formSubmitted = false;
 
   public registerForm = this.fb.group({
-    name: ['', [ Validators.required, Validators.minLength(3) ] ],
-    email: [ '', [ Validators.required, Validators.email ] ],
-    password: [ '', Validators.required ],
-    password2: [ '', Validators.required ],
+    name: ['Leonardo', [ Validators.required, Validators.minLength(3) ] ],
+    email: [ 'leo@gmail.com', [ Validators.required, Validators.email ] ],
+    password: [ '123456', Validators.required ],
+    password2: [ '1234567', Validators.required ],
     terms: [ false, Validators.required ]
+  },{
+    validators: this.samePasswords('password', 'password2')
   });
 
   constructor( private fb: FormBuilder ) { }
@@ -40,8 +42,36 @@ export class RegisterComponent {
     }
   }
 
+  passwordsInvalid() {
+    const pass1 = this.registerForm.get('password')?.value;
+    const pass2 = this.registerForm.get('password2')?.value;
+
+    if ( pass1 !== pass2 && this.formSubmitted ) {
+      return true
+    } else {
+      return false
+    }
+  }
+
   acceptTerms() {
     return !this.registerForm.get('terms')?.value && this.formSubmitted;
+  }
+
+  samePasswords( pass1Name: string, pass2Name: string ) {
+
+    return ( formGroup: FormGroup ) => {
+
+      const pass1Control = formGroup.get(pass1Name);
+      const pass2Control = formGroup.get(pass2Name);
+
+      if( pass1Control?.value === pass2Control?.value ) {
+        pass2Control?.setErrors(null)
+      } else {
+        pass2Control?.setErrors({ notSame: true })
+      }
+
+    }
+
   }
 
 }
