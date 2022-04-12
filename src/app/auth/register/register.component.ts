256041ebd3a531891b1cdeaa6d866611bbb1e979
Leonardo <mr_kunkel@hotmail.com>
2022-04-12 15:02:29 -0500

    Form with validators

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public formSubmitted = false;
 
-  ngOnInit(): void {
+  public registerForm = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ] ],
+    email: [ '', [ Validators.required, Validators.email ] ],
+    password: [ '', Validators.required ],
+    password2: [ '', Validators.required ],
+    terms: [ false, Validators.required ]
+  });
+
+  constructor( private fb: FormBuilder ) { }
+
+  createUser() {
+    this.formSubmitted = true;
+    console.log( this.registerForm.value );
+
+    if ( this.registerForm.valid ) {
+      console.log('Posting form');
+    } else {
+      console.log('Form invalid');
+      
+    }
+  }
+
+  noValid( space: string ): boolean {
+    if( this.registerForm.get(space)?.invalid && this.formSubmitted ) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  acceptTerms() {
+    return !this.registerForm.get('terms')?.value && this.formSubmitted;
   }
 
 }
